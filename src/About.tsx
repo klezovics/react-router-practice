0abbd598664f33bdb08b8e4164c1761e9651d518
artursklezovics <klezovich@gmail.com>
2025-02-05 22:18:23 +0100

    Add example from react router

---
+++ src/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const About = props => {
+    return (
+        <div>
+            <h1>About</h1>
+        </div>
+    );
+};
+
+
+export default About;
